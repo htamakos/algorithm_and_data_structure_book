89b9d67ccb5359b2c1960507a4388d73636c880e
htamakos <hironori.tamakoshi@oracle.com>
2021-01-06 07:28:05 +0900

    fix a little

--- chap07/03_multiply_array/test/SampleClassTest.cpp
+++ chap07/03_multiply_array/test/SampleClassTest.cpp
@@ -10,8 +10,8 @@ TEST(calc, normal) {
   std::vector<long long> a{0, 1, 3, 8, 11, 7, 12};
   std::vector<long long> b{1, 2, 8, 8, 10, 5, 2};
 
-  auto result = min_count<decltype(std::end(a)), long long>(
-      std::end(a), std::end(b), a.size());
-  ASSERT_EQ(13, result);
+  auto result = min_count<decltype(std::begin(a)), long long>(
+      std::begin(a), std::begin(b), a.size());
+  ASSERT_EQ(21, result);
 }
 
